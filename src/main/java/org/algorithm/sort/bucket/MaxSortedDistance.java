92f743ffd1249d291c23a22c2c8aab2ae84d1515
王松迪 <wangsongdi@moqipobing.com>
2024-03-13 13:53:03 +0800

    feat: 1

--- src/main/java/org/algorithm/sort/bucket/MaxSortedDistance.java
+++ src/main/java/org/algorithm/sort/bucket/MaxSortedDistance.java
@@ -1,5 +1,7 @@
 package org.algorithm.sort.bucket;
 
+import java.util.Arrays;
+
 /**
  * <h3>wsd-project</h3>
  * <p>有序最大差值</p>
@@ -10,52 +12,69 @@ package org.algorithm.sort.bucket;
 public class MaxSortedDistance {
 
     public static int getMaxSortedDistance(int[] arr) {
-        int max = arr[0];
+
+        //1 计算桶区间
+        //min = 偏移量
         int min = arr[0];
+        int max = arr[0];
         for (int i : arr) {
-            if (i > max) {
+            if(i > max) {
                 max = i;
             }
-            if (i < min) {
+            if(i < min) {
                 min = i;
             }
         }
+        //d = 桶
         int d = max - min;
-        if(d == 0) {
-            return 0;
-        }
+        System.out.println("1 :"  + d);
 
-        int bucketNum = arr.length;
-        int[][] bucketArr = new int[bucketNum][2];
+
+        //2 创建桶
+        //桶的个数
+        int bucketCount = Math.min(1,  d / arr.length) + 1;
+        //存储桶内最大最小值 [0] = 最小值；[1] = 最大值
+        int[][] thresholdStore = new int[bucketCount][2];
+        for (int[] ints : thresholdStore) {
+            Arrays.fill(ints,  Integer.MAX_VALUE);
+        }
         for (int i : arr) {
-            int bucketIndex = (i - min) * (bucketNum - 1) / d;
-            int x = bucketArr[bucketIndex][0];
-            int n = bucketArr[bucketIndex][1];
-            if(x  == 0 || i > x) {
-                bucketArr[bucketIndex][0] = i;
+            //fixme 计算桶索引；；重点理解
+            int buckedIndex = (i - min) * (bucketCount - 1) / d;
+            System.out.println("3 buckedIndex :" + buckedIndex);
+            if(thresholdStore[buckedIndex][0] == Integer.MAX_VALUE ||  thresholdStore[buckedIndex][0] > i) {
+                thresholdStore[buckedIndex][0] = i;
             }
-            if(n == 0 || i < n) {
-                bucketArr[bucketIndex][1] = i;
+            if(thresholdStore[buckedIndex][1] == Integer.MAX_VALUE || thresholdStore[buckedIndex][1] < i) {
+                thresholdStore[buckedIndex][1] = i;
             }
         }
 
-        int tempMax = 0;
+        System.out.println("4 thresholdStroe : " + Arrays.deepToString(thresholdStore));
+
         int maxDistance = 0;
-        for (int[] ints : bucketArr) {
-            int x = ints[0];
-            int n = ints[1];
-            if(x - tempMax > maxDistance) {
-                maxDistance = x;
+        int leftMax = thresholdStore[0][1];
+        //计算差值
+        for (int[] ints : thresholdStore) {
+            int in = ints[0];
+            int ax = ints[1];
+            //无效桶跳过
+            if(in ==  Integer.MAX_VALUE && ax == Integer.MAX_VALUE) {
+                continue;
             }
+
+            if(in - leftMax > maxDistance) {
+                maxDistance = in - leftMax;
+            }
+
+            leftMax = ax;
         }
 
-
-        return 0;
+        return maxDistance;
     }
 
     public static void main(String[] args) {
-
-        int[] arr = new int[]{2, 3, 10};
-        getMaxSortedDistance(arr);
+        int[] arr = new int[]{-1, 15};
+        System.out.println(getMaxSortedDistance(arr));
     }
 }
